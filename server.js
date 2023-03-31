204c448fa6c371a2fb55897f26cffff1ce5419c9
YASH1730 <yashwantsahu3002@gmail.com>
2023-03-31 20:02:37 +0530

    add :: coupon added

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use('/api/', require('./server/routes'))
 app.use(express.static("frontEnd/build"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'frontEnd', 'build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 })
 
 
