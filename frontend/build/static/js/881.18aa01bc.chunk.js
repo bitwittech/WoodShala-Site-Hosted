8daa63f11ee3c05d18a50f540a76dad35a53d014
YASH1730 <yashwantsahu3002@gmail.com>
2023-04-12 20:59:15 +0530

    fix :: new build new home page

---
+++ frontend/build/static/js/881.18aa01bc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwoodsala_site=self.webpackChunkwoodsala_site||[]).push([[881],{4881:function(e,n,s){s.r(n);s(2791);var t=s(184);n.default=function(){return(0,t.jsxs)(t.Fragment,{children:[(0,t.jsx)("title",{children:"Not Found (404)"}),(0,t.jsx)("center",{style:{marginTop:"5%"},children:(0,t.jsx)("h1",{children:"404 Page Not Found"})})]})}}}]);
+//# sourceMappingURL=881.18aa01bc.chunk.js.map
